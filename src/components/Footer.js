7085e332f1ed4b2fb9cbb08b0e5b5b20bdef990e
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-09-25 18:52:47 +0530

    everything is uptudated its application version 0.0.11

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import {FaFacebook, FaLinkedin, FaTwitter, FaInstagram, FaYoutube, FaGithub} from "react-icons/fa";
-import Massagecard from "../components/Massagecard"
+import React, { useState } from "react";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+  FaYoutube,
+  FaGithub,
+} from "react-icons/fa";
+import Massagecard from "../components/Massagecard";
 
 const Footer = () => {
-  const [email,setEmail] = useState(null)
-  const [massage,setMassage] = useState(null)
+  const [email, setEmail] = useState(null);
+  const [massage, setMassage] = useState(null);
 
   const handleSubmit = (event) => {
-    event.preventDefault() //prevent to auto load the page on submit form event
+    event.preventDefault(); //prevent to auto load the page on submit form event
 
-    setEmail(event.target[0].value)
-    setMassage(event.target[1].value)
-  }
-  const handleClick = () =>{
-    alert("Email: "+email+" Massage: "+massage)
+    setEmail(event.target[0].value);
+    setMassage(event.target[1].value);
+  };
+  const handleClick = () => {
+    if (email === null) {
+      if (massage === null) {
+      }
+    } else {
+      alert("Email: " + email + " Massage: " + massage);
+    }
+
     // return(
     //   <Massagecard />
     // )
-  }
+  };
   return (
     <div className="w-full bg-[#02044A] text-gray-300 py-8 px-2">
       <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
@@ -36,25 +49,22 @@ const Footer = () => {
         <div>
           <h6 className="text-lg font-bold uppercase py-2">Healths</h6>
           <ul>
-            <li className="py-2">Logics</li>
-            <li className="py-2">Programming</li>
-            <li className="py-2">Web Design</li>
-            <li className="py-2">Web Development</li>
-            <li className="py-2">Front-end</li>
-            <li className="py-2">Back-end</li>
-            <li className="py-2">Full-Stacks</li>
+            <li className="py-2">Meditation</li>
+            <li className="py-2">Happiness</li>
+            <li className="py-2">Healthy Foods</li>
+            <li className="py-2">Medical cares</li>
+            <li className="py-2">Awareness</li>
+            <li className="py-2">Hospitals</li>
           </ul>
         </div>
         <div>
           <h6 className="text-lg font-bold uppercase py-2">Daily News</h6>
           <ul>
-            <li className="py-2">Logics</li>
-            <li className="py-2">Programming</li>
-            <li className="py-2">Web Design</li>
-            <li className="py-2">Web Development</li>
-            <li className="py-2">Front-end</li>
-            <li className="py-2">Back-end</li>
-            <li className="py-2">Full-Stacks</li>
+            <li className="py-2">Weather</li>
+            <li className="py-2">Education</li>
+            <li className="py-2">Hawthorns</li>
+            <li className="py-2">Political Events</li>
+            <li className="py-2">Employment</li>
           </ul>
         </div>
         <div className="col-span-2 pt-2  md:pt-4">
@@ -78,7 +88,10 @@ const Footer = () => {
               placeholder="Enter Your Massage..."
               rows={3}
             />
-            <button onClick={handleClick} className="border-none p-2 mb-2 bg-green-600">
+            <button
+              onClick={handleClick}
+              className="border-none p-2 mb-2 bg-green-600"
+            >
               Subscribe
             </button>
           </form>
@@ -97,6 +110,6 @@ const Footer = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
